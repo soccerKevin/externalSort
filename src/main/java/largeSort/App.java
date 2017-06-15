f8ee3eca6947a161c871ab43db5b6bb0922c18b3
soccerkevin <soccerkevint11@gmail.com>
2017-06-15 16:14:53 -0700

    sorted

--- src/main/java/largeSort/App.java
+++ src/main/java/largeSort/App.java
@@ -1,11 +1,26 @@
 package largeSort;
 
-/**
- * Hello world!
- *
- */
+import com.google.code.externalsorting.ExternalSort;
+import java.io.*;
+import java.nio.file.Paths;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
+        try {
+            ExternalSort.mergeSortedFiles(
+                ExternalSort.sortInBatch(new File("./words/words.txt"), (a, b) -> { return a.compareTo(b); }),
+                new File("./words/AtoZ.txt")
+            );
+
+            ExternalSort.mergeSortedFiles(
+                    ExternalSort.sortInBatch(new File("./words/words.txt"), (a, b) -> { return b.compareTo(a); }),
+                    new File("./words/ZtoA.txt")
+            );
+        }catch(Exception e){
+            System.out.print(e.toString());
+        }finally{
+
+        }
     }
 }
